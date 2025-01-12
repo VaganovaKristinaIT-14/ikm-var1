ea4a28075e762f626594d0c27c42b5fc6143c08f
VaganovaKristinaIT-14 <wild.angel.nina@gmail.com>
2025-01-12 20:20:07 +0500

    Delete fun.h

--- fun.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-using namespace std;
-string mult(const string& b);
-void massiv(long int a[], string s, int len, int part);
-string tostr(long int m);
-string sumnum(long int a[], long int b[], int part);
-string dif(long int* a, long int* b, long int* c, int part);
-bool isnum(string a);
